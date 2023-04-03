3510ef880b595f11311dee3bd55acbb32a6d81a1
Oğuz Karan <oguzkaran@csystem.org>
2023-04-03 22:05:41 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,12 +1,56 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Dikkat aşağıdaki örnek azalarak ilerlemez. Dolayısıyla tersten dolaşmak anlamına gelmez
+    Sınıf Çalışması: Parametresi ile aldığı bir sayının basamaklarının basamak sayıncı kuvvetleri toplamının
+    kendisine eşit olup olmadığını test eden isArmstrong isimli fonksiyonu yazınız ve aşağıdaki kod ile test ediniz. Fonksiyon
+    negatif değerler için false değerini döndürecektir
+
+    Açıklama: Kuvvet alma işlemi için bir önceki örnekte yazılan pow fonksiyonu kullanılacaktır
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
-fun main()
-{
-    for (i in 10..1)
-        print("$i ")
+fun main() = runIsArmstrongTest()
 
-    println()
+fun runIsArmstrongTest()
+{
+    while (true) {
+        print("Tabanı giriniz:")
+        val a = readln().toInt()
+
+        if (a == 0)
+            break;
+
+        print("Üssü giriniz:")
+        val b = readLine()!!.toInt()
+
+        println("pow($a, $b) = ${pow(a, b)}")
+    }
+
+    println("Tekrar yapıyor musunuz?")
+}
+
+fun isArmstrong(value: Int) : Boolean
+{
+    TODO("TODO")
+}
+
+fun countDigits(value: Int) : Int
+{
+    var count = 0
+    var temp = value
+
+    do {
+        ++count
+        temp /= 10
+    } while (temp != 0)
+
+    return count
+}
+
+fun pow(a: Int, b: Int) : Int
+{
+    var result = 1
+
+    for (i in 1..b)
+        result *= a
+
+    return result
 }
