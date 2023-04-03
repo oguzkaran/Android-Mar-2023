82b2db56a91e86138da455ac3f96bc44faa3f6f4
Oğuz Karan <oguzkaran@csystem.org>
2023-04-03 21:29:49 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,28 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: Parametresi ile aldığı Int türden bir sayının tersini döndüren reversed isimli fonksiyonu yazınız
-    ve aşağıdaki kod ile test ediniz.
-    Algoritma: 123 -> 3 -> 3 * 10 + 2 = 32 -> 32 * 10 + 1 = 321
+    Dikkat aşağıdaki örnek azalarak ilerlemez. Dolayısıyla tersten dolaşmak anlamına gelmez
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
-fun main() = runReversedTest()
-
-fun runReversedTest()
+fun main()
 {
-    while (true) {
-        print("Bir sayı giriniz:")
-        val value = readln().toInt()
+    for (i in 10..1)
+        print("$i ")
 
-        println("$value sayısının tersi:${reversed(value)}")
-
-        if (value == 0)
-            break
-    }
-
-    println("Tekrar yapıyor musunuz?")
-}
-
-fun reversed(value: Int) : Int
-{
-    TODO("TODO")
+    println()
 }
