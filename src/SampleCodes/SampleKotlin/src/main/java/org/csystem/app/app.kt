6aacba2877d467d2c6f2c66748cf5b641352b9b4
Oğuz Karan <oguzkaran@csystem.org>
2023-04-10 23:05:06 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,42 +1,13 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: Klavyeden katsayıları girilen ikinci dereceden bir denklemin köklerini bulan programı yazınız.
-    Açıklamalar:
-        - if expression yerine when expression kullanılacaktır
+    İki Char toplama işlemine sokulamaz. Yani Char türünün Char türden parametreli toplama (plus) operatör fonksiyonu yoktur
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
-fun main() = runApp()
-
-fun runApp()
+fun main(args:Array<String>)
 {
-    print("a?")
-    val a = readln().toDouble()
+    val c1 = 'D'
+    val c2 = 'Z'
 
-    print("b?")
-    val b = readln().toDouble()
-
-    print("c?")
-    val c = readln().toDouble()
-
-    println(findRoots(a, b, c))
-}
-
-fun calculateDelta(a: Double, b: Double, c: Double) = b * b - 4 * a * c
-
-fun findRoots(a: Double, b: Double, c: Double) : String
-{
-    val delta = calculateDelta(a, b, c)
-
-    fun calculateRoots() : String
-    {
-        val sqrtDelta = Math.sqrt(delta)
-        return "x1 = ${(-b + sqrtDelta) / (2 * a)}, x2 = ${(-b - sqrtDelta) / (2 * a)}"
-    }
-
-    return if (delta > 0)
-        calculateRoots()
-    else if (delta == 0.0)
-        "x1 = x2 = ${-b / (2 * a)}"
-    else
-        "No real root"
+    println(c1.minus(c2))
+    println(c1 - c2)
 }
