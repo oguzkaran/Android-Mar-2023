e5ecf80df7f128ac4ef2f4b5e442d6d52077f33c
Oğuz Karan <oguzkaran@csystem.org>
2023-04-12 23:19:22 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,10 +1,32 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf bildirimi
+    Aşağıdaki örnekte default ctor primary ctor yapılmıştır
 ----------------------------------------------------------------------------------------------------------------------*/
-class SerialPort
+package org.csystem.app
 
-class DeviceInfo
+fun main()
+{
+    var s1 = Sample(10)
+    println("---------------------------")
+    var s2 = Sample(4.5)
+    println("---------------------------")
+    var s3 = Sample()
+    println("---------------------------")
 
-class Person {
     //...
+}
+
+class Sample() {
+    init {
+        println("primary constructor")
+    }
+
+    constructor(b: Double) : this()
+    {
+        println("constructor(Double)")
+    }
+
+    constructor(b: Int) : this(b.toDouble())
+    {
+        println("constructor(Int)")
+    }
 }
