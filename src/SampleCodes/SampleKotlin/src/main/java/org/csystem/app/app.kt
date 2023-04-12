f73e8d913a7d2f72a7deb8e82fc6cb2bf64641f9
Oğuz Karan <oguzkaran@csystem.org>
2023-04-12 22:03:47 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,27 +1,10 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: Klavyeden sıfır girilene kadar alınan Int türden sayıların ortalamasını bulup ekrana yazdıran
-    programı yazınız
+    Sınıf bildirimi
 ----------------------------------------------------------------------------------------------------------------------*/
-package org.csystem.app
+class SerialPort
 
-fun main() = runAverageApplication()
+class DeviceInfo
 
-fun runAverageApplication()
-{
-    var sum = 0
-    var count = 0
-    println("Sayıları girmeye başlayınız:")
-
-    while (true) {
-        val a = readln().toInt()
-
-        if (a == 0)
-            break
-
-        sum += a; ++count
-    }
-
-    println("Ortalama:${sum.toDouble() / count}")
-    println("Tekrar yapıyor musunuz?")
+class Person {
+    //...
 }
-
