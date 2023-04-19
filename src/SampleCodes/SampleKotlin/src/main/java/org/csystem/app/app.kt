9aab521891185d819fcef2984de14097d063a0fc
Oğuz Karan <oguzkaran@csystem.org>
2023-04-19 22:58:35 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,31 +1,16 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: Parametresi ile aldığı iki yazıdan birincisi içerisinde ikincisinden kaç tane olduğunu döndüren
-    countString isimli fonksiyonu ignoreCase parametresi de içerecek şekilde yazınız
+    String sınıfının tüm karakterleri for döngüsü ile elde edilebilir. Yani String sınıfı "iterable"'dır. Iterable kavramı
+    ileride ele alınacaktır
 ----------------------------------------------------------------------------------------------------------------------*/
-package org.csystem.app;
+package org.csystem.app
 
-import kotlin.random.Random
-
-fun main() = runCountStringTest()
-
-
-fun runCountStringTest()
+fun main()
 {
-    while (true) {
-        print("Input the first text:")
-        val s1 = readln()
+    print("Bir yazı giriniz:")
+    val s = readln()
 
-        print("Input the second text:")
-        val s2 = readln()
+    for (ch in s)
+        print("$ch ")
 
-        val ignoreCase = Random.nextBoolean()
-
-        println(if (ignoreCase) "case insensitive" else "case sensitive")
-        println("Count:${countString(s1, s2, ignoreCase)}")
-    }
-}
-
-fun countString(s1: String, s2: String, ignoreCase: Boolean = false) : Int
-{
-    TODO()
+    println()
 }
