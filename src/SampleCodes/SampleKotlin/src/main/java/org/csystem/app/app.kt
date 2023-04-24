bdfdbd95dae8858f8a87a2ba9967952194f28f9b
Oğuz Karan <oguzkaran@csystem.org>
2023-04-24 20:58:37 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,28 +1,9 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: Parametresi ile aldığı bir yazının baş harfini büyük geri kalan harflerini küçük yapan capitalize
-    isimli fonksiyonu yazınız ve test ediniz
-    Örnek: profesyonel Bir Android Programcısı olmak için çok çalışmak gerekir -> Profesyonel bir android programcısı olmak için çok çalışmak gerekir
+
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
-fun main() = runCapitalizeTest()
-
-fun runCapitalizeTest()
+fun main()
 {
-    while (true) {
-        print("Bir yazı giriniz:")
-        val text = readln()
 
-        println(capitalize(text))
-
-        if (text == "elma")
-            break
-    }
-
-    println("Tekrar yapıyor musunuz?")
-}
-
-fun capitalize(s: String) : String
-{
-    TODO()
 }
