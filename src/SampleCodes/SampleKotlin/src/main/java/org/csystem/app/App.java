ec95fe17bd2fa70edc6e4362ab61beb587691c80
Oğuz Karan <oguzkaran@csystem.org>
2023-05-01 21:01:02 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/App.java
@@ -3,11 +3,6 @@ package org.csystem.app;
 class App {
     public static void main(String[] args)
     {
-        int [] a;
-        int [] b;
-
-        b = new int[] {1, 2, 3, 4};
-        a = new int[10];
 
     }
 }
--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,189 +1,34 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: Parametresi ile aldığı gün, ay ve yıl bilgisine ilişkin tarihin haftanın hangi gününe
-    geldiğini döndüren getDayOfWeek global fonksiyonunu aşağıdaki açıklamalara uygun olarak yazınız.
-
-    Açıklama:
-    - Aşağıdaki test kodu ile fonksiyonlarınız için genel bir test yapınız
-    - Programda tarih zamana ilişkin sınıflar kullanılmayacaktır.
-    - getDayOfWeek fonksiyonu 1.1.1900 tarihinden sonraki tarihler için çalışacaktır.
-    - Fonksiyonlar geçersiz bir tarih için -1 değerini döndürecektir
-    - Haftanın günü bilgisi, 1.1.1900 ile verilen tarih arasındaki toplam gün sayısı hesaplanıp 7 değerine
-    göre modu alınarak bulunabilir. Bu değere göre sıfır pazar, 1 pazartesi, ..., 6 değeri de Cumartesi
-    gününe karşılık gelir
-    - Programda dizi kullanılmayacaktır
-    - Aşağıdaki fonksiyonların kesinlikle yazılması koşuluyla istediğiniz fonksiyonu ekleyebilirsiniz.
-    - Yazılmış fonksiyonlar içerisinde değişiklik yapabilirsiniz. Ancak test etmeniz gerektiğini unutmayınız
-    - Çözüm şu ana kadar gördüğümüz konular kullanılarak yapılacaktır
-    - String referansına geri dönen fonksiyonlarda String sınıfını kullanmanız gerekmez. String literal oluştururak
-    yapınız
-
-    (İleride daha iyisi yazılacaktır)
+    enum'ların eşitlik karşılaştırması == veya === operatörü ile yapılabilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
-fun main() = runtTest();
+import org.csystem.util.console.kotlin.readInt
+import kotlin.random.Random
 
-fun runtTest()
+private val g_colors = Color.values()
+
+fun main() = runRandomColorGenerator()
+
+fun runRandomColorGenerator()
 {
-    while (true) {
-        print("Gün?")
-        val day = readInt()
+    val count = readInt("Kaç tane renk üretmek istersiniz:")
 
-        if (day <= 0)
-            break
+    for (i in 1..count) {
+        val c1 = getRandomColor()
+        val c2 = getRandomColor()
 
-        print("Ay?")
-        val month = readInt()
-
-        print("Yıl?")
-        val year = readInt()
-
-        displayDateTR(day, month, year)
-        displayDateEN(day, month, year)
-    }
-
-    println("Tekrar yapıyor musunuz?")
-}
-
-fun readInt() = readln().toInt()
-
-fun displayDateTR(day: Int, month: Int, year: Int)
-{
-    val dayOfWeek = getDayOfWeek(day, month, year)
-
-    println(when {
-        dayOfWeek >= 0 -> "$day ${getMonthNameTR(month)} $year ${getDayOfWeekTR(dayOfWeek)}"
-        else -> "Geçersiz Tarih"
-    })
-}
-
-fun displayDateEN(day: Int, month: Int, year: Int)
-{
-    val dayOfWeek = getDayOfWeek(day, month, year)
-
-    println(when {
-        dayOfWeek >= 0 -> "$day${getDaySuffix(day)} ${getMonthNameEN(month)} $year ${getDayOfWeekEN(dayOfWeek)}"
-        else -> "Invalid Date"
-    })
-}
-
-fun getDaySuffix(day: Int) :String
-{
-    return when (day) {
-        1, 21, 31 -> "st"
-        2, 22 -> "nd"
-        3, 23 -> "rd"
-        else -> "th"
+        println("---------------------------------------------------------------")
+        println("$c1: r = ${c1.r}, g = ${c1.g}, b = ${c1.b}")
+        println("$c2: r = ${c2.r}, g = ${c2.g}, b = ${c2.b}")
+        println(if (c1 === c2) "Aynı renk" else "Farklı renkler")
+        println(if (c1 == c2) "Aynı renk" else "Farklı renkler")
+        println("---------------------------------------------------------------")
     }
 }
 
-fun getDayOfWeekTR(dayOfWeek: Int) : String
-{
-    return when (dayOfWeek) {
-        0 -> "Pazar"
-        1 -> "Pazatesi"
-        2 -> "Salı"
-        3 -> "Çarşamba"
-        4 -> "Perşembe"
-        5 -> "Cuma"
-        else -> "Cumartesi"
-    }
+fun getRandomColor() = g_colors[Random.nextInt(g_colors.size)]
+
+enum class Color(val r: Int = 0, val g: Int = 0, val b: Int = 0) {
+    RED(255), GREEN(g = 255), BLUE(b = 255), WHITE(255, 255, 255), BLACK
 }
-
-fun getMonthNameTR(month: Int) : String
-{
-    return when (month) {
-        1 -> "Ocak"
-        2 -> "Şubat"
-        3 -> "Mart"
-        4 -> "Nisan"
-        5 -> "Mayıs"
-        6 -> "Haziran"
-        7 -> "Temmuz"
-        8 -> "Ağustos"
-        9 -> "Eylül"
-        10 -> "Ekim"
-        11 -> "Kasım"
-        else -> "Aralık"
-    }
-}
-
-fun getDayOfWeekEN(dayOfWeek: Int) : String
-{
-    return when (dayOfWeek) {
-        0 -> "Sun"
-        1 -> "Mon"
-        2 -> "Tue"
-        3 -> "Wed"
-        4 -> "Thu"
-        5 -> "Fri"
-        else -> "Sat"
-    }
-}
-
-fun getMonthNameEN(month: Int) : String
-{
-    return when (month) {
-        1 -> "Jan"
-        2 -> "Feb"
-        3 -> "Mar"
-        4 -> "Apr"
-        5 -> "May"
-        6 -> "Jun"
-        7 -> "Jul"
-        8 -> "Aug"
-        9 -> "Sep"
-        10 -> "Oct"
-        111 -> "Nov"
-        else -> "Dec"
-    }
-}
-
-fun getDayOfWeek(day: Int, month: Int, year: Int) : Int
-{
-    if (year < 1900)
-        return -1
-
-    val totalDays = getDayOfYear(day, month, year)
-
-    if (totalDays == -1)
-        return -1
-
-    return (totalDays + getTotalDays(year)) % 7
-}
-
-fun getTotalDays(year: Int) : Int
-{
-    var totalDays = 0
-
-    for (y in 1900 until year)
-        totalDays += if (isLeapYear(y)) 366 else 365
-
-    return totalDays
-}
-
-fun getDayOfYear(day: Int, month: Int, year: Int) : Int
-{
-    if (!isValidDate(day, month, year))
-        return -1
-
-    var dayOfYear = day
-
-    for (m in (month - 1) downTo 1)
-        dayOfYear += getDaysOfMonth(m, year)
-
-    return dayOfYear
-}
-
-fun getDaysOfMonth(month: Int, year: Int) : Int
-{
-    return when (month) {
-        4, 6, 9, 11 -> 30
-        2 -> if (isLeapYear(year)) 29 else 28
-        else -> 31
-    }
-}
-
-fun isValidDate(day: Int, month: Int, year: Int) = day in 1..31 && month in 1..12 && day <= getDaysOfMonth(month, year)
-
-fun isLeapYear(year: Int) = year % 4 == 0 && year % 100 != 0 || year % 400 == 0
