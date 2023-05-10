810af7bd575270116a0b913370fd30c45d953ecd
Oğuz Karan <oguzkaran@csystem.org>
2023-05-11 00:47:48 +0300

    Update app.kt

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -8,54 +8,6 @@ import kotlin.random.Random
 
 fun main()
 {
-    val count = readInt("Bir sayı giriniz:")
 
-    for (i in 1..count) {
-        val x: A = createRandomA()
-
-        println("---------------------------------------")
-        println(x.javaClass.name)
-
-        if (x is C) {
-            x.c = 10
-            println("x.c = ${x.c}")
-        }
-        else
-            println("Haksız dönüşüm")
-
-        println("---------------------------------------")
-    }
-
-    println("Tekrar yapıyor musunuz?")
 }
 
-fun createRandomA(random: Random = Random) : A
-{
-    return when (random.nextInt(5)) {
-        0 -> B()
-        1 -> C()
-        2 -> D()
-        3 -> E()
-        else -> A()
-    }
-}
-
-open class A {
-    //...
-}
-
-open class B : A() {
-    //...
-}
-
-open class C(var c: Int  = 10) : B() {
-    //...
-}
-
-class D : A() {
-    //...
-}
-
-class E : C() {
-    //..
-}
