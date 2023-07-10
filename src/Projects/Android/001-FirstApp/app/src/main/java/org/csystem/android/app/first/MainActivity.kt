42d67f08308ff9af0b74bd59d16d7dee1af9ff09
Oğuz Karan <oguzkaran@csystem.org>
2023-07-10 20:58:05 +0300

    Update

--- src/Projects/Android/001-FirstApp/app/src/main/java/org/csystem/android/app/first/MainActivity.kt
+++ src/Projects/Android/001-FirstApp/app/src/main/java/org/csystem/android/app/first/MainActivity.kt
@@ -1,20 +1,24 @@
 package org.csystem.android.app.first
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import java.time.DateTimeException
-import java.time.LocalDate
+import androidx.appcompat.app.AppCompatActivity
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private fun displayDateTime()
+    {
+        val now = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy kk:mm:ss")
+
+        Toast.makeText(this, formatter.format(now), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val today = LocalDate.now()
-        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-
-        Toast.makeText(this, formatter.format(today), Toast.LENGTH_SHORT).show()
+        displayDateTime()
     }
 }
