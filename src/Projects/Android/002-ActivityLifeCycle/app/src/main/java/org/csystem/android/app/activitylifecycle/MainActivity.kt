40f521372489eb84c45c5154b1e3d3c12f232bd0
Oğuz Karan <oguzkaran@csystem.org>
2023-07-10 22:46:28 +0300

    Update

--- src/Projects/Android/002-ActivityLifeCycle/app/src/main/java/org/csystem/android/app/activitylifecycle/MainActivity.kt
+++ src/Projects/Android/002-ActivityLifeCycle/app/src/main/java/org/csystem/android/app/activitylifecycle/MainActivity.kt
@@ -1,11 +1,37 @@
 package org.csystem.android.app.activitylifecycle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, R.string.on_create_message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart()
+    {
+        Toast.makeText(this, R.string.on_start_message, Toast.LENGTH_SHORT).show()
+        super.onStart()
+    }
+
+    override fun onRestart()
+    {
+        Toast.makeText(this, R.string.on_restart_message, Toast.LENGTH_SHORT).show()
+        super.onRestart()
+    }
+    override fun onStop()
+    {
+        Toast.makeText(this, R.string.on_stop_message, Toast.LENGTH_SHORT).show()
+        super.onStop()
+    }
+
+    override fun onDestroy()
+    {
+        Toast.makeText(this, R.string.on_destroy_message, Toast.LENGTH_SHORT).show()
+        super.onDestroy()
     }
 }
