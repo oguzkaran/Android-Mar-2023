8c9fa291d62315c614612085380a654ea09e3677
Oğuz Karan <oguzkaran@csystem.org>
2023-07-12 20:58:46 +0300

    Update

--- src/Projects/Android/002-ActivityLifeCycle/app/src/main/java/org/csystem/android/app/activitylifecycle/MainActivity.kt
+++ src/Projects/Android/002-ActivityLifeCycle/app/src/main/java/org/csystem/android/app/activitylifecycle/MainActivity.kt
@@ -5,8 +5,15 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private fun displayInitMessage(bundle: Bundle?)
+    {
+        val message = if (bundle == null) "First Created" else "Re created by system"
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
     override fun onCreate(savedInstanceState: Bundle?)
     {
+        displayInitMessage(savedInstanceState)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, R.string.on_create_message, Toast.LENGTH_SHORT).show()
@@ -18,10 +25,22 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
     }
 
+    override fun onResume()
+    {
+        Toast.makeText(this, R.string.on_resume_message, Toast.LENGTH_SHORT).show()
+        super.onResume()
+    }
+
     override fun onRestart()
     {
         Toast.makeText(this, R.string.on_restart_message, Toast.LENGTH_SHORT).show()
         super.onRestart()
+    }
+
+    override fun onPause()
+    {
+        Toast.makeText(this, R.string.on_pause_message, Toast.LENGTH_SHORT).show()
+        super.onPause()
     }
     override fun onStop()
     {
