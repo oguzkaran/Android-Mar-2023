7e005591871cd1d278bad13fe81d0ae691642cf6
Oğuz Karan <oguzkaran@csystem.org>
2023-07-28 22:20:07 +0300

    Update

--- src/Projects/Android/BasicViewsDataBinding/app/src/main/java/org/csystem/android/app/basicviews/MainActivity.kt
+++ src/Projects/Android/BasicViewsDataBinding/app/src/main/java/org/csystem/android/app/basicviews/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.databinding.DataBindingUtil
 import org.csystem.android.app.basicviews.databinding.ActivityMainBinding
 import org.csystem.android.app.basicviews.global.alert.promptDecision
 import org.csystem.android.app.basicviews.global.alert.promptNotConfirmedDialog
+import org.csystem.android.app.basicviews.viewmodel.MainActivityViewModel
 import org.csystem.android.app.basicviews.viewmodel.RegisterInfo
 import java.time.DateTimeException
 import java.time.LocalDate
@@ -48,8 +49,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun getRegisterInfo() : RegisterInfo?
     {
-        val password = mBinding.registerInfoViewModel!!.password
-        if (!confirm(password, mBinding.confirmPassword!!))
+        if (!confirm(mBinding.password!!, mBinding.confirmPassword!!))
             return null
 
         val name = mBinding.registerInfoViewModel!!.name
@@ -57,63 +57,18 @@ class MainActivity : AppCompatActivity() {
         val birthDate = LocalDate.of(mBinding.year!!.toInt(), mBinding.month!!.toInt(), mBinding.day!!.toInt())
         val userName = mBinding.registerInfoViewModel!!.userName
 
-        return RegisterInfo(name, email, birthDate, userName, password)
-    }
-
-    private fun registerButtonClickedCallback()
-    {
-        try {
-           val registerInfo = getRegisterInfo()
-
-           if (registerInfo != null) {
-               Toast.makeText(this, registerInfo.toString(), Toast.LENGTH_LONG).show()
-               mBinding.result = registerInfo.toString()
-           }
-        }
-        catch (ignore: NumberFormatException) {
-            Toast.makeText(this, R.string.message_number_format_exception, Toast.LENGTH_LONG).show()
-        }
-        catch (ignore: DateTimeException) {
-           Toast.makeText(this, R.string.message_datetime_exception, Toast.LENGTH_LONG).show()
-        }
-    }
-
-    private fun closeButtonClickedCallback()
-    {
-        promptDecision(this, R.string.confirm_close_alert_dialog_title, R.string.message_confirm_close,
-            R.string.yes_button_text, R.string.no_button_text,
-            {_, _ -> positiveButtonClickedCallback()}) {_, _ -> negativeButtonClickedCallback()}
+        return RegisterInfo(name, email, birthDate, userName, mBinding.password!!)
     }
 
     private fun clearEditTexts()
     {
-        mBinding.registerInfoViewModel!!.password = ""
+        mBinding.password = ""
         for (view in mBinding.mainActivityLinearLayoutMain.children) {
             if (view is EditText)
                 view.setText("")
         }
-        mBinding.result = ""
-    }
 
-    private fun clearButtonClickedCallback()
-    {
-        clearEditTexts()
-        initBirthDateTexts()
-        mBinding.accept = false
-        mBinding.passwordInputType = INPUT_TYPE_TEXT_PASSWORD_HIDE
-        mBinding.showPasswordButtonText = resources.getString(R.string.button_show_password_text)
-        mBinding.show = false
-        mBinding.mainActivityEditTextName.requestFocus()
-    }
-
-    private fun showPasswordButtonClickedCallback()
-    {
-        val show = mBinding.show as Boolean
-        val resId = if (show) R.string.button_hide_password_text else R.string.button_show_password_text
-
-        mBinding.showPasswordButtonText = resources.getString(resId)
-        mBinding.passwordInputType = if (show) INPUT_TYPE_TEXT_PASSWORD_SHOW else INPUT_TYPE_TEXT_PASSWORD_HIDE
-        mBinding.show = !show
+        mBinding.confirmPassword = ""
     }
 
     private fun initBirthDateTexts()
@@ -129,8 +84,10 @@ class MainActivity : AppCompatActivity() {
     {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         mBinding.registerInfoViewModel = RegisterInfo()
+        mBinding.mainActivityViewModel = MainActivityViewModel(this)
         mBinding.show = true
         mBinding.passwordInputType = INPUT_TYPE_TEXT_PASSWORD_HIDE
+        mBinding.showPasswordButtonText = resources.getString(R.string.button_show_password_text)
     }
 
     private fun initViews()
@@ -148,5 +105,52 @@ class MainActivity : AppCompatActivity() {
     {
         super.onCreate(savedInstanceState)
         initialize()
+    }
+
+    fun showPasswordButtonClicked()
+    {
+        val show = mBinding.show as Boolean
+        val resId = if (show) R.string.button_hide_password_text else R.string.button_show_password_text
+
+        mBinding.showPasswordButtonText = resources.getString(resId)
+        mBinding.passwordInputType = if (show) INPUT_TYPE_TEXT_PASSWORD_SHOW else INPUT_TYPE_TEXT_PASSWORD_HIDE
+        mBinding.show = !show
+    }
+
+    fun registerButtonClicked()
+    {
+        try {
+            val registerInfo = getRegisterInfo()
+
+            if (registerInfo != null) {
+                Toast.makeText(this, registerInfo.toString(), Toast.LENGTH_LONG).show()
+                mBinding.result = registerInfo.toString()
+            }
+        }
+        catch (ignore: NumberFormatException) {
+            Toast.makeText(this, R.string.message_number_format_exception, Toast.LENGTH_LONG).show()
+        }
+        catch (ignore: DateTimeException) {
+            Toast.makeText(this, R.string.message_datetime_exception, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun clearButtonClicked()
+    {
+        clearEditTexts()
+        initBirthDateTexts()
+        mBinding.accept = false
+        mBinding.passwordInputType = INPUT_TYPE_TEXT_PASSWORD_HIDE
+        mBinding.showPasswordButtonText = resources.getString(R.string.button_show_password_text)
+        mBinding.show = true
+        mBinding.result = ""
+        mBinding.mainActivityEditTextName.requestFocus()
+    }
+
+    fun closeButtonClicked()
+    {
+        promptDecision(this, R.string.confirm_close_alert_dialog_title, R.string.message_confirm_close,
+            R.string.yes_button_text, R.string.no_button_text,
+            {_, _ -> positiveButtonClickedCallback()}) {_, _ -> negativeButtonClickedCallback()}
     }
 }
---
+++ src/Projects/Android/BasicViewsDataBinding/app/src/main/java/org/csystem/android/app/basicviews/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,14 @@
+package org.csystem.android.app.basicviews.viewmodel
+
+import org.csystem.android.app.basicviews.MainActivity
+
+class MainActivityViewModel(val activity: MainActivity) {
+    fun handleShowPasswordButton() = activity.showPasswordButtonClicked()
+
+    fun handleRegisterButton() = activity.registerButtonClicked()
+
+    fun handleClearButton() = activity.clearButtonClicked()
+
+    fun handleCloseButton() = activity.closeButtonClicked()
+
+}
