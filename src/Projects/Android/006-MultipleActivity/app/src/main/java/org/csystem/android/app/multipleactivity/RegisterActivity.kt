cc962089a10e32b0c1b45c7cb65a1c63f6cbdefe
Oğuz Karan <oguzkaran@csystem.org>
2023-08-07 18:06:24 +0300

    Update RegisterActivity.kt

--- src/Projects/Android/006-MultipleActivity/app/src/main/java/org/csystem/android/app/multipleactivity/RegisterActivity.kt
+++ src/Projects/Android/006-MultipleActivity/app/src/main/java/org/csystem/android/app/multipleactivity/RegisterActivity.kt
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class RegisterActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
     }
