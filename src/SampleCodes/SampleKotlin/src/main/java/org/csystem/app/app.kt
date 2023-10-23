0eba3d0e9784bde2496849ac2000e0f492a3f89c
Oğuz Karan <oguzkaran@csystem.org>
2023-10-23 22:26:36 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,21 +1,46 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: LocalDate sınıfını kullanarak iki tarih arasındaki toplam yılı Double türden çıkartma operatörü
-    ile hesaplayan kodları yazınız
+    Bir thread başka bir yarattığında hangi thread'in önce çizelmeye gireceği o anki duruma bağlıdır. Yani hangisinin
+    önce çizelgelemeye girmesi belirsizdir. (Hatta ayrı CPU ya da çekirdekte olurlarsa gerçekten aynı anda da
+    çizelgelemeye girebilirler). Thread'lerin stack alanları birbirinden ayrıdır. Ancak heap alanı ortaktır. Dolayısıyla
+    aynı nesne üzerinde birden fazla thread'in işlem yapması durumunda bir takım problemler söz konusu olabilmektedir.
+    Bu problemlere tipik olan senkronizsyon problemleri denir. İleride bir takım senkronizasyon problemleri ve çözümleri
+    uygulamalar içerisinde ele alınacaktır. Aşağıdaki örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
-import java.time.LocalDate
-import java.time.Month
-import java.time.temporal.ChronoUnit
-
-operator fun LocalDate.minus(localDate: LocalDate) = ChronoUnit.DAYS.between(localDate, this) / 365.0
+import org.csystem.util.console.kotlin.readInt
+import org.csystem.util.console.kotlin.readLong
+import org.csystem.util.console.kotlin.readString
+import org.csystem.util.string.kotlin.getRandomTextEN
+import java.io.*
+import java.nio.charset.StandardCharsets
+import kotlin.random.Random
 
 fun main()
 {
-    val birthDate = LocalDate.of(1976, Month.SEPTEMBER, 10)
-    val today = LocalDate.now()
+    val basePath = readString("Input base path:")
+    val count = readInt("Input files count:")
+    val dataCount = readLong("Input count per each:")
 
-    val age = today - birthDate
+    (1..count).forEach {TextGeneratorThread(dataCount, File("$basePath-${it}.txt").absolutePath).apply { start() }}
+}
 
-    println("Age:$age")
+class TextGeneratorThread(private val mCount: Long, private val mPath: String, private val mMin: Int = 5, private val mBound: Int = 11, private val mRandom: Random = Random) : Thread() {
+
+    private fun generateCallback(fos: FileOutputStream)
+    {
+        BufferedWriter(OutputStreamWriter(fos, StandardCharsets.UTF_8)).use { bw ->
+            (0..<mCount).forEach { bw.write("${mRandom.getRandomTextEN(mRandom.nextInt(mMin, mBound))}\r\n") }
+        }
+    }
+
+    override fun run()
+    {
+        try {
+            FileOutputStream(mPath).use {generateCallback(it)}
+        }
+        catch (ex: IOException) {
+            println("IO Problem:${ex.message}")
+        }
+    }
 }
