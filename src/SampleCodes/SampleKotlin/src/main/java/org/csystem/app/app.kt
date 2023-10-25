21356622ed4e0c84aff9413d841fc227154191d6
Oğuz Karan <oguzkaran@csystem.org>
2023-10-25 20:24:23 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,10 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Bir thread başka bir yarattığında hangi thread'in önce çizelmeye gireceği o anki duruma bağlıdır. Yani hangisinin
-    önce çizelgelemeye girmesi belirsizdir. (Hatta ayrı CPU ya da çekirdekte olurlarsa gerçekten aynı anda da
-    çizelgelemeye girebilirler). Thread'lerin stack alanları birbirinden ayrıdır. Ancak heap alanı ortaktır. Dolayısıyla
-    aynı nesne üzerinde birden fazla thread'in işlem yapması durumunda bir takım problemler söz konusu olabilmektedir.
-    Bu problemlere tipik olan senkronizsyon problemleri denir. İleride bir takım senkronizasyon problemleri ve çözümleri
-    uygulamalar içerisinde ele alınacaktır. Aşağıdaki örneği inceleyiniz
+    Runnable Java'da functional interface olduğundan Kotlin'de de lambda fonksiyonlar ile kullanılabilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
@@ -16,31 +11,32 @@ import java.io.*
 import java.nio.charset.StandardCharsets
 import kotlin.random.Random
 
+private fun generateCallback(fos: FileOutputStream, count: Long, random: Random, min: Int, bound: Int)
+{
+    BufferedWriter(OutputStreamWriter(fos, StandardCharsets.UTF_8)).use { bw ->
+        (0..<count).forEach { _ -> bw.write("${random.getRandomTextEN(random.nextInt(min, bound))}\r\n") }
+    }
+}
+
+
 fun main()
 {
     val basePath = readString("Input base path:")
     val count = readInt("Input files count:")
     val dataCount = readLong("Input count per each:")
 
-    (1..count).forEach {TextGeneratorThread(dataCount, File("$basePath-${it}.txt").absolutePath).apply { start() }}
+    File(basePath).parentFile.mkdirs()
+
+    (1..count).forEach {Thread{ generateTextsCallback(dataCount, File("$basePath-${it}.txt").absolutePath, 5, 11) }
+        .apply { start() }}
 }
 
-class TextGeneratorThread(private val mCount: Long, private val mPath: String, private val mMin: Int = 5, private val mBound: Int = 11, private val mRandom: Random = Random) : Thread() {
-
-    private fun generateCallback(fos: FileOutputStream)
-    {
-        BufferedWriter(OutputStreamWriter(fos, StandardCharsets.UTF_8)).use { bw ->
-            (0..<mCount).forEach { bw.write("${mRandom.getRandomTextEN(mRandom.nextInt(mMin, mBound))}\r\n") }
-        }
+fun generateTextsCallback(count: Long, path: String, min: Int, bound: Int, random: Random = Random)
+{
+    try {
+        FileOutputStream(path).use {generateCallback(it, count, random, min, bound)}
     }
-
-    override fun run()
-    {
-        try {
-            FileOutputStream(mPath).use {generateCallback(it)}
-        }
-        catch (ex: IOException) {
-            println("IO Problem:${ex.message}")
-        }
+    catch (ex: IOException) {
+        println("IO Problem:${ex.message}")
     }
 }
