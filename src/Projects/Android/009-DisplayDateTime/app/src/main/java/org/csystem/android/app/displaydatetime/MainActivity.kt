3346bf29dabc15e736fdf2016ebc1f957e9f6ab7
Oğuz Karan <oguzkaran@csystem.org>
2023-11-13 17:07:38 +0300

    Update MainActivity.kt

--- src/Projects/Android/009-DisplayDateTime/app/src/main/java/org/csystem/android/app/displaydatetime/MainActivity.kt
+++ src/Projects/Android/009-DisplayDateTime/app/src/main/java/org/csystem/android/app/displaydatetime/MainActivity.kt
@@ -71,7 +71,7 @@ class MainActivity : AppCompatActivity() {
         mChronoTimer = Timer()
         mChronoTimer.scheduleAtFixedRate(createChronoTimerTask(), 0, 1000)
     }
-    
+
     private fun startAutoDisplayChronometer()
     {
         mBinding.mainActivityChronometerAutoDisplay.start()
