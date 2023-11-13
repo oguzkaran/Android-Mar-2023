a3c1baa1d8e29daded7dc1e0c2e57d82bff20a2f
Oğuz Karan <oguzkaran@csystem.org>
2023-11-13 17:13:50 +0300

    Update

--- src/Projects/Android/PaymentApplication/DataServiceLib/build.gradle.kts
+++ src/Projects/Android/PaymentApplication/DataServiceLib/build.gradle.kts
@@ -37,14 +37,14 @@ android {
 
 dependencies {
     implementation(project(path = ":RepositoryLib"))
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     implementation ("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-compiler:2.48")
