11d102c4c9fb4ae025815dc686bab8e7bf335873
Oğuz Karan <oguzkaran@csystem.org>
2023-11-22 20:56:18 +0300

    Update

--- src/Projects/Android/011-RandomGenerator/app/src/main/java/org/csystem/android/app/generator/random/MainActivity.kt
+++ src/Projects/Android/011-RandomGenerator/app/src/main/java/org/csystem/android/app/generator/random/MainActivity.kt
@@ -1,15 +1,23 @@
 package org.csystem.android.app.generator.random
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
 import org.csystem.android.app.generator.random.databinding.ActivityMainBinding
+import org.csystem.android.app.generator.random.global.what.WHAT_SCHEDULER_CALLBACK_EXCEPTION
+import org.csystem.android.app.generator.random.global.what.WHAT_SCHEDULER_TRACK_CALLBACK_EXCEPTION
+import org.csystem.android.app.generator.random.global.what.WHAT_TEXT_ADAPTER_ADD
+import org.csystem.android.app.generator.random.global.what.WHAT_WAIT_SCHEDULER_TIMEOUT
 import org.csystem.android.app.generator.random.string.generateRandomTextEN
 import org.csystem.android.app.generator.random.viewmodel.data.GenerateInfo
 import org.csystem.android.app.generator.random.viewmodel.listener.MainActivityListenerViewModel
+import java.lang.ref.WeakReference
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
@@ -21,9 +29,48 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
     private lateinit var mScheduledFuture: ScheduledFuture<*>
+    private lateinit var mHandler: Handler
 
     @Inject
     lateinit var executorService: ScheduledExecutorService
+
+    private class SchedulerHandler(activity: MainActivity) : Handler(Looper.myLooper()!!) {
+        private val mWeakReference = WeakReference(activity)
+
+        private fun handleTextAdapterAdd(text: String)
+        {
+            //...
+            mWeakReference.get()!!.mBinding.adapter!!.add(text)
+        }
+
+        private fun handleSchedulerTrackCallbackException(str: String)
+        {
+            //...
+            Toast.makeText(mWeakReference.get(), "Problem occurred in scheduler track thread:$str", Toast.LENGTH_LONG).show()
+        }
+
+        private fun handleSchedulerCallbackException(str: String)
+        {
+            //...
+            Toast.makeText(mWeakReference.get(), "Problem occurred in scheduler:$str", Toast.LENGTH_LONG).show()
+        }
+
+        private fun handleSchedulerTimeout()
+        {
+            //...
+            Toast.makeText(mWeakReference.get(), "Text(s) Generated!...", Toast.LENGTH_LONG).show()
+        }
+
+        override fun handleMessage(msg: Message)
+        {
+            when (msg.what) {
+                WHAT_TEXT_ADAPTER_ADD -> handleTextAdapterAdd(msg.obj as String)
+                WHAT_SCHEDULER_TRACK_CALLBACK_EXCEPTION -> handleSchedulerTrackCallbackException(msg.obj as String)
+                WHAT_SCHEDULER_CALLBACK_EXCEPTION -> handleSchedulerCallbackException(msg.obj as String)
+                WHAT_WAIT_SCHEDULER_TIMEOUT -> handleSchedulerTimeout()
+            }
+        }
+    }
 
     private fun waitScheduler()
     {
@@ -32,6 +79,7 @@ class MainActivity : AppCompatActivity() {
         }
         catch (_: TimeoutException) {
             mScheduledFuture.cancel(false)
+            mHandler.sendEmptyMessage(WHAT_WAIT_SCHEDULER_TIMEOUT)
         }
     }
 
@@ -49,7 +97,7 @@ class MainActivity : AppCompatActivity() {
             waitScheduler()
         }
         catch (ex: Throwable) {
-            runOnUiThread{Toast.makeText(this, "Poblem occurred in scheduler track thread:${ex.message}", Toast.LENGTH_LONG).show()}
+            mHandler.sendMessage(mHandler.obtainMessage(WHAT_SCHEDULER_TRACK_CALLBACK_EXCEPTION, ex.message))
         }
         finally {
             mBinding.enabled = true
@@ -59,19 +107,13 @@ class MainActivity : AppCompatActivity() {
     private fun schedulerCallback()
     {
         try {
-            runOnUiThread {
-                mBinding.adapter!!.add(
-                    generateRandomTextEN(
-                        Random.nextInt(
-                            mBinding.generateInfo!!.minimum,
-                            mBinding.generateInfo!!.maximum + 1
-                        )
-                    )
-                )
-            }
+            val count = Random.nextInt(mBinding.generateInfo!!.minimum, mBinding.generateInfo!!.maximum + 1)
+            val text = generateRandomTextEN(count)
+
+            mHandler.sendMessage(Message.obtain(mHandler, WHAT_TEXT_ADAPTER_ADD, text))
         }
         catch (ex: Throwable) {
-            runOnUiThread{Toast.makeText(this, "Poblem occurred in scheduler:${ex.message}", Toast.LENGTH_LONG).show()}
+            mHandler.sendMessage(mHandler.obtainMessage(WHAT_SCHEDULER_CALLBACK_EXCEPTION, ex.message))
         }
     }
 
@@ -92,6 +134,7 @@ class MainActivity : AppCompatActivity() {
     private fun initialize()
     {
         initBinding()
+        mHandler = SchedulerHandler(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?)
