b06665f3e4d2fe4fa8f51e5f20ac83ad837e0451
Oğuz Karan <oguzkaran@csystem.org>
2023-12-28 23:59:21 +0300

    Update

--- src/Projects/Android/012-GeonamesWikiSearchRetfofit copy/app/src/main/java/org/csystem/android/app/geonames/wikisearch/application/GeonamesWikiSearchApplication.kt
+++
@@ -1,7 +0,0 @@
-package org.csystem.android.app.geonames.wikisearch.application
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class GeonamesWikiSearchApplication : Application()
