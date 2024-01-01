787f2d8e4022134b75f82e4077eda1cf51955dc4
Oğuz Karan <oguzkaran@csystem.org>
2024-01-01 21:24:39 +0300

    Update

--- src/Projects/Android/012-GeonamesWikiSearchRetfofit/app/src/main/java/org/csystem/android/app/geonames/wikisearch/MainActivity.kt
+++ src/Projects/Android/012-GeonamesWikiSearchRetfofit/app/src/main/java/org/csystem/android/app/geonames/wikisearch/MainActivity.kt
@@ -14,27 +14,85 @@ import org.csystem.android.app.geonames.wikisearch.viewmodel.MainActivityViewMod
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStreamWriter
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.concurrent.ExecutorService
 import javax.inject.Inject
+
+const val SHARED_PREF_FILE_NAME = "geonames-input"
+const val LAST_OPEN_BEFORE = "LAST_OPEN_BEFORE"
+const val Q = "Q"
+const val MAX_ROWS = "MAX_ROWS"
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
+    private val mWikiInfo =  ArrayList<WikiInfo>()
 
     @Inject
     lateinit var wikiSearchService: IGeonamesWikiSearchService
 
-    private fun initData()
+    @Inject
+    lateinit var dateTimeFormatter : DateTimeFormatter
+
+    @Inject
+    lateinit var threadPool : ExecutorService
+
+    private fun saveData(data: String?, bw: BufferedWriter)
+    {
+        try {
+            bw.write("$data\r\n");
+            bw.flush()
+        }
+        catch (ex: IOException) {
+            runOnUiThread{Toast.makeText(this, "IO Error while saving:${ex.message}", Toast.LENGTH_LONG).show()}
+        }
+    }
+
+    private fun saveCache(bw: BufferedWriter)
+    {
+        val count = mWikiInfo.size
+
+        for (i in 1..<count)
+            saveData(mWikiInfo[i].summary, bw)
+    }
+
+    private fun saveCacheCallback()
+    {
+        try {
+            BufferedWriter(OutputStreamWriter(FileOutputStream(File(cacheDir, "data.dat")))).use(this::saveCache)
+        }
+        catch (ex: IOException) {
+            runOnUiThread{Toast.makeText(this, "IO Error while opening:${ex.message}", Toast.LENGTH_LONG).show()}
+        }
+    }
+
+    private fun loadData()
+    {
+        val sharedPrefs = getSharedPreferences(SHARED_PREF_FILE_NAME, MODE_PRIVATE)
+        val prefs = getPreferences(MODE_PRIVATE)
+
+        mBinding.lastOpenBefore = "Last Open:${prefs.getString(LAST_OPEN_BEFORE, "")}"
+        mBinding.q = sharedPrefs.getString(Q, "zonguldak")
+        mBinding.maxRows = sharedPrefs.getInt(MAX_ROWS, 10)
+    }
+
+    private fun initBoundData()
     {
         mBinding.viewModel = MainActivityViewModel(this)
-        mBinding.wikiInfoAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, ArrayList<WikiInfo>())
-        mBinding.q = "zonguldak"
-        mBinding.maxRows = 10
+        mBinding.wikiInfoAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, mWikiInfo)
+        loadData()
     }
 
     private fun initBinding()
     {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        initData()
+        initBoundData()
     }
 
     private fun initialize()
@@ -46,6 +104,20 @@ class MainActivity : AppCompatActivity() {
     {
         super.onCreate(savedInstanceState)
         initialize()
+    }
+
+    override fun onDestroy()
+    {
+        val editorPrefs = getPreferences(MODE_PRIVATE).edit()
+        val editorSharedPrefs = getSharedPreferences(SHARED_PREF_FILE_NAME, MODE_PRIVATE).edit()
+
+        editorSharedPrefs.putString(Q, mBinding.q)
+        editorSharedPrefs.putInt(MAX_ROWS, mBinding.maxRows)
+        editorSharedPrefs.apply() //apply asenkron kaydeder. Senkron kaydetmek için commit metodu çağrılabilir
+
+        editorPrefs.putString(LAST_OPEN_BEFORE, dateTimeFormatter.format(LocalDateTime.now()))
+        editorPrefs.apply()
+        super.onDestroy()
     }
 
     fun getButtonClicked()
@@ -61,6 +133,7 @@ class MainActivity : AppCompatActivity() {
                 if (wikiSearch?.wikiInfo != null) {
                     wikiSearch.wikiInfo.forEach { mBinding.wikiInfoAdapter!!.add(it) }
                     mBinding.wikiInfoAdapter!!.notifyDataSetChanged()
+                    threadPool.execute{saveCacheCallback()}
                 }
                 else
                     Toast.makeText(this@MainActivity, "Error in service", Toast.LENGTH_LONG).show()
---
+++ src/Projects/Android/012-GeonamesWikiSearchRetfofit/app/src/main/java/org/csystem/android/app/geonames/wikisearch/api/di/module/datetime/DateTimeFormatterModule.kt
@@ -0,0 +1,16 @@
+package org.csystem.android.app.geonames.wikisearch.api.di.module.datetime
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import java.time.format.DateTimeFormatter
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DateTimeFormatterModule {
+    @Provides
+    @Singleton
+    fun create() : DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy kk:mm:ss")
+}
