8bc5f6f92f1d6cce3b8fc091f3d6ac894fd472ba
Oğuz Karan <oguzkaran@csystem.org>
2024-05-21 01:47:20 +0300

    Update build.gradle.kts

--- src/Projects/Android/PaymentSystem/PaymentAndroidService/build.gradle.kts
+++ src/Projects/Android/PaymentSystem/PaymentAndroidService/build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.0" apply false
+    id("com.android.application") version "8.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("com.android.library") version "8.4.0" apply false
+    id("com.android.library") version "8.4.1" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
 }
