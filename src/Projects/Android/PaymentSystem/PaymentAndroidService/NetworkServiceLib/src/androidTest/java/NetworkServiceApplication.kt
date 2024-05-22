b2a743cceea2a5d0a7834637ab2de64e916eafbe
Oğuz Karan <oguzkaran@csystem.org>
2024-05-22 23:03:57 +0300

    Update

---
+++ src/Projects/Android/PaymentSystem/PaymentAndroidService/NetworkServiceLib/src/androidTest/java/NetworkServiceApplication.kt
@@ -0,0 +1,75 @@
+
+import com.karandev.util.net.TcpUtil
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.io.IOException
+import java.net.Socket
+import javax.inject.Inject
+
+const val HOST = "161.97.141.113"
+const val PORT = 50540
+class NetworkServiceApplication @Inject constructor() {
+
+    private fun socketConnect(id : Byte) : String
+    {
+        var msg : String
+        try {
+            Socket(HOST, PORT).use {
+                TcpUtil.sendByte(it, id)
+                val status = TcpUtil.receiveByte(it)
+                 msg = statusMessage(status,it)
+            }
+
+        }
+        catch (ex : IOException)
+        {
+            throw IOException("socketConnect IoException failed")
+        }
+        catch (ex : Throwable)
+        {
+            throw  Throwable("socketConnect Throwable  failed")
+        }
+        return msg
+    }
+    private fun statusMessage(status : Byte, socket: Socket ) : String
+    {
+
+        try {
+            return if (status == 1.toByte())
+                TcpUtil.receiveStringViaLength(socket)
+            else if (status == 0.toByte())
+                     "yanlış bilgi"
+            else
+                 "Bilinmeyen bir hata oluştu"
+        }
+        catch (ex : IOException)
+        {
+            throw IOException("statusMessage IoException")
+        }
+        catch (ex : Throwable)
+        {
+            throw  Throwable("statusMessage ThrowableException")
+        }
+
+
+    }
+    @OptIn(DelicateCoroutinesApi::class)
+     fun run(id : Byte) : String
+    {
+        var msg : String = ""
+        runBlocking {
+            GlobalScope.launch {
+                msg = socketConnect(id)
+            }
+        }
+
+
+        return msg
+
+
+    }
+
+
+}
--- src/Projects/Android/PaymentSystem/PaymentAndroidService/NetworkServiceLib/src/androidTest/java/org/csystem/android/app/payment/network/ExampleInstrumentedTest.kt
+++ src/Projects/Android/PaymentSystem/PaymentAndroidService/NetworkServiceLib/src/androidTest/java/org/csystem/android/app/payment/network/ExampleInstrumentedTest.kt
@@ -1,12 +1,12 @@
 package org.csystem.android.app.payment.network
 
+
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +15,14 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
+    fun useAppContext()
+    {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.csystem.android.app.payment.network.test", appContext.packageName)
+        assertEquals("com.karandev.util.net", appContext.packageName)
     }
+
+
+
 }
